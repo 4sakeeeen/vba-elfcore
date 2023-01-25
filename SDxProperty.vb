3e5b955b5e952b1e5a515929b544598255e0031b
4sakeeeen <91375491+4sakeeeen@users.noreply.github.com>
2023-01-25 10:34:41 +0500

    Create SDxProperty.vb

---
+++ SDxProperty.vb
@@ -0,0 +1,11 @@
+Public Name As String
+Public Value As String
+Public InterfaceName As String
+
+Sub Init(PropInterfaceName As String, PropName As String, PropValue As String)
+
+InterfaceName = PropInterfaceName
+Name = PropName
+Value = PropValue
+
+End Sub
